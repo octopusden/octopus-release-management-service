037caf17297ad4eb7245db9a4e6788d51e18148f
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-04-02 18:50:50 +0300

    Filters for BuildController (#5)
    * Filters for BuildController
    
    * Fix tests

--- server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/legacy/LegacyRelengClient.kt
+++ server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/legacy/LegacyRelengClient.kt
@@ -1,14 +1,16 @@
 package org.octopusden.octopus.releasemanagementservice.legacy
 
 import feign.Param
+import feign.QueryMap
 import feign.RequestLine
 import org.octopusden.octopus.releasemanagementservice.client.common.dto.BuildDTO
+import org.octopusden.octopus.releasemanagementservice.client.common.dto.BuildFilterDTO
 import org.octopusden.octopus.releasemanagementservice.client.common.dto.ShortBuildDTO
 
 interface LegacyRelengClient {
 
     @RequestLine("GET rest/release-engineering/3/component/{component}/builds")
-    fun getBuilds(@Param("component") component: String): Collection<ShortBuildDTO>
+    fun getBuilds(@Param("component") component: String, @QueryMap filter: BuildFilterDTO): Collection<ShortBuildDTO>
 
     @RequestLine("GET rest/release-engineering/3/component/{component}/version/{version}/build")
     fun getBuild(@Param("component") component: String, @Param("version") version: String): BuildDTO
--- server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/service/BuildService.kt
+++ server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/service/BuildService.kt
@@ -1,9 +1,10 @@
 package org.octopusden.octopus.releasemanagementservice.service
 
 import org.octopusden.octopus.releasemanagementservice.client.common.dto.BuildDTO
+import org.octopusden.octopus.releasemanagementservice.client.common.dto.BuildFilterDTO
 import org.octopusden.octopus.releasemanagementservice.client.common.dto.ShortBuildDTO
 
 interface BuildService {
-    fun getBuilds(component: String): Collection<ShortBuildDTO>
+    fun getBuilds(component: String, filter: BuildFilterDTO): Collection<ShortBuildDTO>
     fun getBuild(component: String, version: String): BuildDTO
 }
