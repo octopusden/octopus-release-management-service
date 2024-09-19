bdc67b15f9c21de41d10f4f1c678e66de200dee7
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-09-19 09:55:00 +0300

    Extract LegacyRelengClient to separate module (#15)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -99,6 +99,7 @@ dependencyManagement {
 
 dependencies {
     implementation(project(":common"))
+    implementation(project(":legacy-releng-client"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
