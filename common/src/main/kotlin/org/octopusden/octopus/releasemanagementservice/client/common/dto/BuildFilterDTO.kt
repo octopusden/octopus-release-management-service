6806a45fc0ad67c5ea628454b7fee886a9a62f13
Dmitriy Ustinov <dustinov.mbox@gmail.com>
2024-11-12 14:23:57 +0300

    Add line versions to build filter DTO (#17)

--- common/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/common/dto/BuildFilterDTO.kt
+++ common/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/common/dto/BuildFilterDTO.kt
@@ -3,6 +3,7 @@ package org.octopusden.octopus.releasemanagementservice.client.common.dto
 data class BuildFilterDTO(
     val statuses: Set<BuildStatus> = emptySet(),
     val minors: Set<String> = emptySet(),
+    val lines: Set<String> = emptySet(),
     val versions: Set<String> = emptySet(),
     val inReleaseBranch: Boolean? = null,
     val descending: Boolean = false,
