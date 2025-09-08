813709b2fa9723f53f79625d1128a81767407e6f
Mark Rode <rode.mark01@gmail.com>
2025-09-08 19:36:27 +0300

    add new parameter

--- common/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/common/dto/MandatoryUpdateFilterDTO.kt
+++ common/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/common/dto/MandatoryUpdateFilterDTO.kt
@@ -3,5 +3,6 @@ package org.octopusden.octopus.releasemanagementservice.client.common.dto
 data class MandatoryUpdateFilterDTO(
     val activeLinePeriod: Int,
     val excludeComponents: Set<String> = emptySet(),
-    val excludeSystems: Set<String> = emptySet()
+    val excludeSystems: Set<String> = emptySet(),
+    val isFullMatchSystems: Boolean = true
 )
