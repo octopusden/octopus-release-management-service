77211d91b94714a385ab51f7b0b8f6ab3d0509d8
Mark Rode <54143576+rodemark@users.noreply.github.com>
2025-10-06 13:22:05 +0300

    Parameterization of the client timeout (#37)

--- client/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/impl/ReleaseManagementServiceClientParametersProvider.kt
+++ client/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/client/impl/ReleaseManagementServiceClientParametersProvider.kt
@@ -3,4 +3,6 @@ package org.octopusden.octopus.releasemanagementservice.client.impl
 interface ReleaseManagementServiceClientParametersProvider {
     fun getApiUrl(): String
     fun getTimeRetryInMillis(): Int
+    fun getConnectTimeoutInMillis(): Int
+    fun getReadTimeoutInMillis(): Int
 }
