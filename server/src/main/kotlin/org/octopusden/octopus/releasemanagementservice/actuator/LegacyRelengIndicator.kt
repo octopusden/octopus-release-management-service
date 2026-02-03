83346d060ee0954d0c583cf39a0385c55acbebd1
adellinekania <adellinekaniasetiyawan@gmail.com>
2026-02-03 13:11:59 +0700

    rollback

--- server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/actuator/LegacyRelengIndicator.kt
+++ server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/actuator/LegacyRelengIndicator.kt
@@ -1,31 +1,23 @@
 package org.octopusden.octopus.releasemanagementservice.actuator
 
 import org.octopusden.octopus.releasemanagementservice.legacy.LegacyRelengClient
-import org.springframework.boot.actuate.availability.ReadinessStateHealthIndicator
-import org.springframework.boot.actuate.health.Health.Builder
-import org.springframework.boot.availability.ApplicationAvailability
-import org.springframework.boot.availability.ReadinessState.ACCEPTING_TRAFFIC
-import org.springframework.boot.availability.ReadinessState.REFUSING_TRAFFIC
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.stereotype.Component
 
 @Component
 class LegacyRelengIndicator(
-    private val legacyRelengClient: LegacyRelengClient, availability: ApplicationAvailability?
-): ReadinessStateHealthIndicator(availability) {
-    override fun doHealthCheck(builder: Builder) {
-        try {
+    private val legacyRelengClient: LegacyRelengClient
+): HealthIndicator {
+    override fun health(): Health {
+        return try {
             legacyRelengClient.getHealth()
-            builder
-                .up()
-                .withDetail("legacyReleng", "Available")
-                .withDetail("readinessState", ACCEPTING_TRAFFIC)
-
+            Health.up().build()
         } catch (e: Exception) {
-            builder
-                .down()
-                .withDetail("legacyReleng", "Unavailable")
+            Health.down()
+                .withDetail("message", "Legacy Releng Service is unavailable")
                 .withException(e)
-                .withDetail("readinessState", REFUSING_TRAFFIC)
+                .build()
         }
     }
 }
