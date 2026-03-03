46b801e1c6372ae2632b6804371b6ee1515094e8
Mark Rode <rode.mark01@gmail.com>
2026-03-03 22:38:23 +0300

    make cache ttl and size configurable

--- server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/config/CacheConfig.kt
+++ server/src/main/kotlin/org/octopusden/octopus/releasemanagementservice/config/CacheConfig.kt
@@ -1,6 +1,7 @@
 package org.octopusden.octopus.releasemanagementservice.config
 
 import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
@@ -10,12 +11,17 @@ import java.util.concurrent.TimeUnit
 @Configuration
 class CacheConfig {
     @Bean
-    fun cacheManager(): CacheManager {
+    fun cacheManager(
+        @Value("\${cache.detailed-component.ttl-minutes:10}")
+        ttlMinutes: Long,
+        @Value("\${cache.detailed-component.maximum-size:1000}")
+        maximumSize: Long
+    ): CacheManager {
         val cacheManager = CaffeineCacheManager("detailedComponentCache")
         cacheManager.setCaffeine(
             Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.MINUTES)
-                .maximumSize(1_000)
+                .expireAfterWrite(ttlMinutes, TimeUnit.MINUTES)
+                .maximumSize(maximumSize)
         )
         return cacheManager
     }
